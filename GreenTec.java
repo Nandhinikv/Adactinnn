d395eb0b59db3f71bb0dae58620b8f2220b6ee77
nandhinikg <135498569+nandhinikg@users.noreply.github.com>
2023-06-04 11:20:50 +0400

    updated coding with conditions

--- GreenTec.java
+++ GreenTec.java
@@ -17,6 +17,17 @@ public class GreenTec {
 		system.out.println("name"+name+);
 	}
 		
+		public class Condition {
+	public static void main(String[] args) {
+		for (int i = 3; i >=1; i--) {
+			for (int j = 3; j >=0; j--) {
+				System.out.print(5);	}
+			System.out.println();
+		}
+	}
+
+}
+
 	
 	public static void main(String[] args) {
 		GreenTec va = new GreenTec();
